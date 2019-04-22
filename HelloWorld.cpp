ff9f6e23d55705f6a2dd15e944c647491ef882c2
lxl2000417 <3503074718@qq.com>
2019-04-22 14:52:42 +0800

    再次提交helloworld

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,10 @@
 #include "HelloWorld.h"
 #include <iostream>
 
-void HelloWorld::hello() {
+int main(){
+	int a=0,b=2; 
   std::cout << "Hello World!" << std::endl;
+  std::cout << a+b << std::endl;
+  return 0;
 }
 
